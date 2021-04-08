f0568deae0c4a5f13636c3e1f02bb515900d32f7
s4tenko <s4tenko@gmail.com>
2021-04-08 14:10:09 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,48 @@
+"use strict";
+
+class RangeValidator {
+  constructor(from, to) {
+    this.from = from;
+    this.to = to;
+  }
+  set from(value) {
+    if (typeof value !== "number") {
+      throw new TypeError("Incorrect variable type!");
+    }
+    this._from = value;
+  }
+  get from() {
+    return this._from;
+  }
+  set to(value) {
+    if (typeof value !== "number") {
+      throw new TypeError("Incorrect variable type!");
+    }
+    this._to = value;
+  }
+  get to() {
+    return this._to;
+  }
+  get getRangeLimits() {
+    return [this.from, this.to];
+  }
+  get getRangeArray() {
+    let newArray = [];
+    if (this.to < 0) {
+      for (let i = this.from; i >= this.to; i--) {
+        newArray.push(i);
+      }
+    } else {
+      for (let i = this.from; i <= this.to; i++) {
+        newArray.push(i);
+      }
+    }
+    return newArray;
+  }
+  validate(number) {
+    if (this.getRangeArray.includes(number)) return number;
+    throw new RangeError("Number is out of range!");
+  }
+}
+
+const test = new RangeValidator(10, -20);
